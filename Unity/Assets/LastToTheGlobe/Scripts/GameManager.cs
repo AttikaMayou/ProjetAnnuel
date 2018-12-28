9945cfd427d54b8520021fc0871f6cbfa0459a9c
Attika <attika.mayou@gmail.com>
2018-12-28 01:25:13 +0100

    PUN Tuto - Part 3
    Add new Rooms + Game Manager prefab

---
+++ Unity/Assets/LastToTheGlobe/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LastToTheGlobe.Scripts
+{
+    public class GameManager : MonoBehaviour
+    {
+        
+        #region Photon Messages
+
+        /// <summary>
+        /// Called when the player left the room. We need to load the Launcher Scene.
+        /// </summary>
+        public void OnLeftRoom()
+        {
+            //TODO : handle this properly (not by index)
+            //See the SceneSwitcher script in ressources
+            SceneManager.LoadScene(0);
+        }
+        
+        #endregion
+        
+        #region Public Methods
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+        
+        #endregion
+
+    }
+}
